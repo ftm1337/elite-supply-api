65a2b4ccc354e5d908c6366c63ff8386be3acddd
ftm1337 <84405345+ftm1337@users.noreply.github.com>
2023-01-23 16:03:43 -0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,7 +1,8 @@
 
 async function geti() {
-  let rd = await fetch("https://api.coingecko.com/api/v3/global")
-  return rd.json();
+  let rd = //await fetch("https://api.coingecko.com/api/v3/global")
+      await fetch("https://rpc.ankr.com/fantom", {"credentials": "omit","headers": {"Accept": "*/*","Accept-Language": "en-US,en;q=0.5","content-type": "application/json"},"body": "{\"method\":\"eth_call\",\"params\":[{\"to\":\"0x3a603eceae046828febbcbd097bf97adc23dc072\",\"data\":\"0x370158ea\"},\"latest\"],\"id\":96,\"jsonrpc\":\"2.0\"}","method": "POST","mode": "cors"});
+  return (await rd.json()).result;
 }
 
 
